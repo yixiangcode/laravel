981d6d1df5d97f6aeee9882b82c0d562190a6f20
Cheng Yi Xiang <yixiang972@gmail.com>
2024-12-30 11:44:53 +0800

    30/12/2024

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,18 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Stripe;
 
+use App\Models\myCart;
+use App\Models\myOrder;
+use DB;
+use Auth;
+
 class PaymentController extends Controller
 {
     public function paymentPost(Request $request)
     {       
-	Stripe\Stripe::setApiKey(env('sk_test_51QXBvGKyJnPHGD3gkefWguEIPVh7IC6ZhtV7h3IeKGscO7cMqz6xBlu68TcRy9loHvby27mxnMOBBNuLLffSoQpt00U2vMiYhZ'));
+	Stripe\Stripe::setApiKey('sk_test_51QXBvGKyJnPHGD3gkefWguEIPVh7IC6ZhtV7h3IeKGscO7cMqz6xBlu68TcRy9loHvby27mxnMOBBNuLLffSoQpt00U2vMiYhZ');
         Stripe\Charge::create ([
                 "amount" => $request->sub*100,   // RM10  10=10 cen 10*100=1000 cen
                 "currency" => "MYR",
                 "source" => $request->stripeToken,
                 "description" => "This payment is testing purpose of southern online",
         ]);
-           
+        
+        $newOrder = myOrder::Create([
+            'paymentStatus' => "Done",
+            'userID' => Auth::id(),
+            'amount' => $request->sub,
+        ]);
+        $orderID=DB::table('my_orders')->where('userID',
+        '=', Auth::id())->orderBy('created_at','desc')
+        ->first(); // get last record from the user
+
+        $items=$request->input('cid',[]);
+        foreach($items as $item=>$value){
+            $carts=myCart::find($value);
+            $carts->orderID=$orderID->id;
+            $carts->save();
+        }
+
         return back();
     }
 
---
+++ app/Notifications/orderPaid.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class orderPaid extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Hi, you received a new order')
+                    ->action('Check Order', url('https://www.southernOnline.com'))
+                    ->line('Thank you for using our Southern Online!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
